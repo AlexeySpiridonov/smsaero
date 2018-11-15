83b540352686e02c281ae557c05d4573431e7401
Anton Kucherov <idexter.ru@gmail.com>
2018-11-16 01:53:39 +0300

    Alpha version of client

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # smsaero
 
 SMSAero API Client (Go)
+
+## Example
+
+```go
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gitlab.com/hiteam/smsaero"
+)
+
+func main() {
+	client := smsaero.NewClient("e-mail", "api-secret", "", "")
+	msg := smsaero.MessageRequest{
+		Numbers: []string{"79871234567"},
+		Sign: "hightech+",
+		Text: "Ваш код: 123456",
+		Channel: smsaero.ChannelDirect,
+	}
+	resp, err := client.Send(msg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%#v\n", resp)
+}
+```
---
+++ models.go
@@ -0,0 +1,40 @@
+package smsaero
+
+import "time"
+
+type Status struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
+
+type AuthResponse struct {
+	Status
+	Data interface{} `json:"data"`
+}
+
+type SendResponse struct {
+	Status
+	Data []SmsMessage `json:"data"`
+}
+
+type SmsMessage struct {
+	ID           int64   `json:"id"`
+	From         string  `json:"from"`
+	Number       string  `json:"number"`
+	Text         string  `json:"text"`
+	Status       int64   `json:"status"`
+	ExtendStatus string  `json:"extendStatus"`
+	Channel      string  `json:"channel"`
+	Cost         float64 `json:"cost"`
+	DateCreate   int64   `json:"dateCreate"`
+	DateSend     int64   `json:"dateSend"`
+}
+
+type MessageRequest struct {
+	Numbers     []string   // numbers array Обязательно (на выбор)	Номера телефонов
+	Sign        string     // sign	string	Обязательно	Подпись отправителя
+	Text        string     // text	string	Обязательно	Текст сообщения
+	Channel     string     // channel	string	Обязательно	Канал отправки
+	DateSend    *time.Time // dateSend	integer	Не обязательно	Дата для отложенной отправки сообщения (в формате unixtime)
+	CallbackUrl string     // callbackUrl	string	Не обязательно	url для отправки статуса сообщения в формате http://your.site или https://your.site (в ответ система ждет ста
+}
